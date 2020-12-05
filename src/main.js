e99d56eab1e692c6df1d320402ed5f31857a16e3
Miguel Santos <msantos11@ucol.mx>
2020-12-04 19:49:26 -0600

    implemented sumarParesDo

--- src/main.js
+++ src/main.js
@@ -40,6 +40,17 @@ export default class App {
     }
     return impares;
   }
+  sumarParesDo() {
+    let i = 1;
+    let suma = 0;
+    do {
+      if (i % 2 == 0) {
+        suma = suma + i;
+      }
+      i++;
+    } while (i <= 20);
+    return suma;
+  }
 }
 
 let app = new App();
@@ -51,3 +62,5 @@ console.log(app.contarImparesFor(5, 9));
 console.log(app.sumarParesWhile());
 
 console.log(app.contarImparesWhile(5, 11));
+
+console.log(app.sumarParesDo());
